f1657f5f4f7198c519c3aeb8ce8985e555b01080
PAUL BOURY <paulboury17@gmail.com>
2024-05-30 12:34:56 +0200

    Workshop finished

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const Fighter = require("./src/Fighter.js");
+const Weapon = require("./src/Weapon.js");
+const Shield = require("./src/Shield.js");
+
+/** Create a weapon */
+const sword = new Weapon("Sword", 10);
+
+/** Create a shield */
+const shield = new Shield("Shield", 10);
 
 /** Create Heracles  */
-const heracles = new Fighter("🧔 Heracles", 20, 6);
+const heracles = new Fighter("🧔 Heracles", 20, 6, sword, shield);
 
 /** Create the opponent  */
 const boar = new Fighter("🐗 Erymanthian Boar", 25, 12);
+
+
 
 /**
  * Helper to produce the result of a round
@@ -25,3 +35,26 @@ const score = (fighter1, fighter2) => {
     loser: fighter1
   };
 };
+
+// Let's fight !!!
+{
+  let round = 0;
+
+  while (heracles.isAlive() && boar.isAlive()) {
+      console.log(`🕛 Round #${round}`);
+
+      heracles.fight(boar);
+      console.log(roundDisplay(heracles, boar));
+
+      boar.fight(heracles);
+      console.log(roundDisplay(boar, heracles));
+
+      round++;
+  }
+
+  const result = score(heracles, boar);
+
+  console.log(`💀 ${result.loser.name} is dead`);
+  console.log(`🏆 ${result.winner.name} wins (💙 ${result.winner.life} )`);
+}
+
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,23 +1,48 @@
 const MAX_LIFE = 100;
 
 class Fighter {
-    constructor(name, strength, dexterity) {
+    constructor(name, strength, dexterity, weapon=null, shield=null) {
         this.name = name;
         this.strength = strength;
         this.dexterity = dexterity;
         this.life = MAX_LIFE;
+        this.weapon = weapon;
+        this.shield = shield;
+    }
+
+    // method to get the damage
+    getDamage() {
+        let totalDamage = 0;
+        if (this.weapon !== null) {
+            totalDamage = this.strength + this.weapon.damage;
+        } else {
+            totalDamage = this.strength;
+        }
+        return totalDamage;
+    }
+
+    // method to get the defense
+    getDefense() {
+        let totalDefense = 0;
+        if (this.shield !== null) {
+            totalDefense = this.dexterity + this.shield.protection;
+        } else {
+            totalDefense = this.dexterity;
+        }
+        return totalDefense;
     }
 
 
     // Launch a fight
     fight(defender) {
-        const attackPoints = this.getRandomInt(this.strength);
+        const attackPoints = this.getRandomInt(this.getDamage());
 
-        const damages = Math.max(attackPoints - defender.dexterity, 0);
+        const damages = Math.max(attackPoints - defender.getDefense(), 0);
 
         defender.life = Math.max(defender.life - damages, 0);
     }
 
+    
 
     // Generate a random value between 1 and max
     getRandomInt(max) {
